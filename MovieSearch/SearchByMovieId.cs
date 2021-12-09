37d8802313966ad2c735b10641c5b2928d2fee99
Wilma Hammarshall <wilmahammarshall@gmail.com>
2021-12-09 17:29:12 +0100

    söka med id
    id

--- MovieSearch/SearchByMovieId.cs
+++ MovieSearch/SearchByMovieId.cs
@@ -8,32 +8,45 @@ namespace MovieSearch
 {
     public class SearchByMovieId
     {
-        public static HttpClient client = new HttpClient();
+        private static HttpClient client = new HttpClient();
 
         public static async Task<Movie> SearchById()
         {
-            int id = 0;
-
+            
             Console.Write("Enter movie id: ");
-            id = int.Parse(Console.ReadLine());
+            int id = int.Parse(Console.ReadLine());
 
             try
             {
                 string uriId = $"https://api.themoviedb.org/3/movie/{id}?api_key=7a2b47ee7ece23d9547cabff80a87234";
                 var response = await client.GetAsync(uriId);
+
                 response.EnsureSuccessStatusCode();
 
                 var responseContent = await response.Content.ReadAsStringAsync();
 
-                Movie thismovie = JsonConvert.DeserializeObject<Movie>(responseContent);
+                Movie movie = JsonConvert.DeserializeObject<Movie>(responseContent);
+                
 
-                return thismovie;
+                Console.WriteLine("\nHomepage: {0}", movie.Homepage);
+                //Console.WriteLine("\nId: ", movie.Id);
+                Console.WriteLine("\nLanguage: {0}", movie.Original_language);
+                Console.WriteLine("\nTitle: {0}", movie.Original_title);
+                Console.WriteLine("\nOverview: {0}", movie.Overview);
+                //Console.WriteLine(”\nPoster path: {0}”, movie.Poster_path);
+                Console.WriteLine("\nRelease date: {0}", movie.Release_date);
+                Console.WriteLine("\nRuntime: {0}", movie.Runtime);
+                Console.WriteLine("\nVote average: {0}", movie.Vote_average);
+
+                return movie;
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
+                return null;
             }
-
+            
         }
+
     }
 }
