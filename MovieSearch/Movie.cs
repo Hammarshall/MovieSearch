88d92970192bd94e5415cee979adc72aacdf497a
Wilma Hammarshall <wilmahammarshall@gmail.com>
2021-12-15 09:24:42 +0100

    done

--- MovieSearch/Movie.cs
+++ MovieSearch/Movie.cs
@@ -8,14 +8,14 @@ namespace MovieSearch
 {
     public class Movie
     {
-        public string Homepage { get; set; }
+        public string? Homepage { get; set; }
         public int Id { get; set; }
         public string Original_language { get; set; }
         public string Original_title { get; set; }
         public string Overview { get; set; }
         public string Poster_path { get; set; }
         public string Release_date { get; set; }
-        public int Runtime { get; set; }
+        public int? Runtime { get; set; }
         public double Vote_average { get; set; }
     }
 }
